b65afd6dd089de852a94345378c32fceb6f74b3a
slowiakjakub <dwabas9@gmail.com>
2020-10-22 15:42:01 +0200

    Implement Decoding method

---
+++ MusicTagLibrary/APIHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicTagLibrary
+{
+    public class APIHelper
+    {
+        public static void SetApplicationClientKey()
+        {
+            AcoustID.Configuration.ClientKey = "pYEBEoFX0R";
+        }
+
+    }
+}
---
+++ MusicTagLibrary/DecodingProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcoustID;
+
+namespace MusicTagLibrary
+{
+    class DecodingProcessor
+    {
+        public static string GetFingerprintFromFile(string file)
+        {
+            AudioBuffer bfr = new AudioBuffer();
+            NAudioDecoder decoder = new NAudioDecoder(file);
+
+            decoder.Decode(bfr, decoder.Length);
+
+            //At this point mp3 file is decoded into bfr.data[] 
+
+            ChromaContext ctx = new ChromaContext();
+
+            ctx.Start(decoder.SampleRate, decoder.Channels);
+            ctx.Feed(bfr.data, bfr.data.Length);
+            ctx.Finish();
+
+            string fp = ctx.GetFingerprint();
+
+            return fp;
+        }
+    }
+}
