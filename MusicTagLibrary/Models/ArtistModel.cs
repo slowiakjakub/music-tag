7484cb917ef944d382f65972a89e58be96f7be70
slowiakjakub <dwabas9@gmail.com>
2020-10-22 15:48:04 +0200

    Add basic data models

---
+++ MusicTagLibrary/Models/ArtistModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicTagLibrary.Models
+{
+    public class ArtistModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MusicTagLibrary/Models/RecordingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicTagLibrary.Models
+{
+    public class RecordingModel
+    {
+        public List<ArtistModel> Artists { get; set; }
+        public string Title;
+    }
+}
