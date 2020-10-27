481b25bd0c850b13a7f046d03d54c4799e5bfd21
slowiakjakub <dwabas9@gmail.com>
2020-10-27 17:24:15 +0100

    Quick Fix

--- MusicTagLibrary/Models/ReleaseGroupModel.cs
+++ MusicTagLibrary/Models/ReleaseGroupModel.cs
@@ -8,7 +8,7 @@ namespace MusicTagLibrary.Models
 {
     public class ReleaseGroupModel
     {
-        public List<SecondaryType> secondaryTypes { get; set; }
+        public List<string> secondaryTypes { get; set; }
         public string Title { get; set; }
         public string Type { get; set; }
     }
