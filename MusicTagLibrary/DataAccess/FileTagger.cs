07aa6d7ec5d9f28ea0ea58f162d07e1a61667dc4
slowiakjakub <dwabas9@gmail.com>
2020-10-30 01:10:52 +0100

    Create TagModel

--- MusicTagLibrary/DataAccess/FileTagger.cs
+++ MusicTagLibrary/DataAccess/FileTagger.cs
@@ -18,7 +18,7 @@ namespace MusicTagLibrary.DataAccess
             this.lookupResponse = lookupResponse;
         }
 
-        public void TagFile()
+        public void TagFile() //TODO - Method is deprecated
         {
             var tfile = TagLib.File.Create(filePath);
 
@@ -61,6 +61,49 @@ namespace MusicTagLibrary.DataAccess
                         }
                     }
                     tfile.Save();
+                }
+            }
+        }
+        private void ValidTagPossibilites()
+        {
+            ModelValidator modelValidator = new ModelValidator(lookupResponse);
+
+            bool isValidResults = false;
+            List<LookupResultModel> validResults = modelValidator.TryGetValidLookupResults(out isValidResults);
+
+            if (isValidResults)
+            {
+                bool isValidRecordings = false;
+                List<RecordingModel> validRecordings = modelValidator.TryGetValidRecordings(validResults.First().Recordings, out isValidRecordings);
+                if (isValidRecordings)
+                {
+
+                    canTagTitle = true;
+                    canTagPerformers = true;
+
+                    RecordingModel firstValidRecording = validRecordings.First();
+                    List<ReleaseGroupModel> foundSongReleaseGroups = modelValidator.GetReleaseGroupsBySpecificArtists(firstValidRecording.ReleaseGroups, firstValidRecording.Artists);
+
+                    if (foundSongReleaseGroups.Count > 0)
+                    {
+                        ReleaseGroupModel foundSongFirstReleaseGroup = foundSongReleaseGroups.First();
+
+                        if (foundSongFirstReleaseGroup.Type.Equals("Album"))
+                        {
+                            if (foundSongFirstReleaseGroup.Title != null)
+                            {
+                                canTagAlbumName = true;
+                            }
+                        }
+                        bool isValidReleases = false;
+                        List<ReleaseModel> validReleases = modelValidator.TryGetValidReleases(foundSongFirstReleaseGroup.Releases, out isValidReleases);
+
+                        if (isValidReleases)
+                        {
+                            ReleaseModel firstValidRelease = validReleases.First();
+                            canTagYear = true;
+                        }
+                    }
                 }
             }
         }
