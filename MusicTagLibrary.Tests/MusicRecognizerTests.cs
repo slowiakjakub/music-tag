be7ed82ef0338992a4e9b038e542f31af319895c
slowiakjakub <dwabas9@gmail.com>
2020-12-09 22:32:40 +0100

    Start implementing test for MusicRecognizer

---
+++ MusicTagLibrary.Tests/MusicRecognizerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MusicTagLibrary.Tests
+{
+    public class MusicRecognizerTests
+    {
+        [Fact]
+        public async void RunMusicTagForAudioFileAsync_ShouldWorkWithMp3Song()
+        {
+            //Arrange
+            string filePath = $@"{Directory.GetCurrentDirectory()}\TestSet\SongsMp3\prelude.mp3";
+            string expected = "Kevin MacLeod- Prelude";
+
+            //Act
+            await MusicRecognizer.RunMusicTagForAudioFileAsync(filePath);
+
+            //Assert
+            Assert.Equal(expected, "test");
+        }
+    }
+}
