8ef5891bbc89accd927787b6d186b566fcfbfb72
slowiakjakub <dwabas9@gmail.com>
2020-12-09 11:39:13 +0100

    Add AudioTooShortException

---
+++ MusicTagLibrary/Exceptions/AudioTooShortException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicTagLibrary.Exceptions
+{
+    [Serializable]
+    public class AudioTooShortException : Exception
+    {
+        public AudioTooShortException() { }
+
+        public AudioTooShortException(string message)
+            : base(message) { }
+
+        public AudioTooShortException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
